af6365c1c01712080295388a5eb17495035ef6f5
Igor Freitas <igorlo.freitas@hotmail.com>
2024-01-13 20:09:25 -0300

    Renderização do lado do servidor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
+// pages/_app.tsx
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
